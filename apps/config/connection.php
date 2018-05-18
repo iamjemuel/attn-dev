71a3e32e4f1111f1d9a12ee36c3a14d95cc0b58f
Jemuel Acuna <jemuel.acuna@gigamare.com>
2018-05-18 12:04:23 +0800

    config changes

--- apps/config/connection.php
+++ apps/config/connection.php
@@ -1,14 +1,15 @@
 <?php
-// define('HOST', 'localhost');
-// define('USER', 'root');
-// define('PASS', '');
-// define('DB', 'db_attendance');
+// $HOST = 'localhost';
+// $USER = 'root';
+// $PASS = 'localpass';
+// $DB = 'db_attendance';
+
 $HOST = 'mysql.hostinger.ph';
 $USER = 'u300307657_sys';
 $PASS = 'Light@2018';
 $DB = 'u300307657_attn';
 		$con = new mysqli($HOST, $USER, $PASS, $DB);
-    	
+
 	    if ($con->connect_error) {
 	        die("Not Connected" .$dbc->connect_error);
 	    }else{
--- apps/variables.php
+++ apps/variables.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $leader = array('No lifegroup leader', 'Ronald Acuna', 'Alvin Madronio', 'Albert Madronio', 'Glenn Corpuz', 'Mark Christian De Omania', 'Arjuna Aguilar', 'Mel Aguilar', 'Carla Baldomero', 'Mechole Williams', 'Dorothy Magbuhos', 'Jenny Madronio', 'Stephanie Cadiong', 'Robert/Mike', 'Clariza Fortuna', 'Elena Encio', 'Jessica Calanog', 'Restie Cadiong');
 $type = array('Member', 'Visitor');
 $slots = array('All', '7AM', '9AM', '11AM', '2PM', '4PM', '6PM', '8PM', '10PM');
